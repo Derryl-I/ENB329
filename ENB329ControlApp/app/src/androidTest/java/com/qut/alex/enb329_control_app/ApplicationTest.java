eaac009b1cbb7af7fc21e4f0f6706f0328a1cab7
scorpioncoder <alexjbarnier@gmail.com>
2016-08-09 09:36:50 +1000

    Created Empty Project
    Empty project to add to and experiment with.

---
+++ ENB329ControlApp/app/src/androidTest/java/com/qut/alex/enb329_control_app/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.qut.alex.enb329_control_app;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
